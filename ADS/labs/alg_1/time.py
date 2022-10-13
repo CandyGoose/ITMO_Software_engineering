79153b99fc9afb2c3b40c56e10d64f2cf32fbc95
Kasianenko Vera Mikhailovna <112972833+VeraKasianenko@users.noreply.github.com>
2022-10-13 21:44:00 +0300

    Delete ADS/labs directory

--- ADS/labs/alg_1/time.py
+++
@@ -1,4 +0,0 @@
-import timeit
-
-print(timeit.timeit(stmt='inverse([[2,0,0], [0,-3,0], [0,0,1]])', setup='from test_time import inverse', number=10000))
-print(timeit.timeit(stmt='inverse_numpy([[2,0,0], [0,-3,0], [0,0,1]])', setup='from test_time import inverse_numpy', number=10000))
