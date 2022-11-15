0d6b9f1a89a8b50e2082b0bd83d42825a3b24fe4
Pelmeshka <112972833+VeraKasianenko@users.noreply.github.com>
2022-11-16 00:51:23 +0300

    delete baza, fix find

--- Programming/prog_dz/dz.py
+++ Programming/prog_dz/dz.py
@@ -276,18 +276,21 @@ def delete_baza():
     otvet = input('Какой продукт желаете удалить? ')
     otvet = otvet.capitalize()
     if otvet != '':
-        if otvet not in cur_list:
-            print('Ошибка. Такой записи нет.')
+        for i in range(len(baza)):
+            baza_prod = baza[i][0].split(' ')
+            if otvet == baza_prod[0]:
+                for k in range(len(baza)):
+                    if otvet in (baza[k - 1][0]):
+                        baza.pop(k - 1)
+                with open('products.txt', 'w', encoding='UTF-8') as baz:
+                    for i in range(len(baza)):
+                        baz.write(baza[i][0])
+                        if i != len(baza) - 1:
+                            baz.write('\n')
+                print('Продукт успешно удален из базы!')
+                break
         else:
-            for k in range(len(baza)):
-                if otvet in (baza[k - 1][0]):
-                    baza.pop(k - 1)
-            with open('products.txt', 'w', encoding='UTF-8') as baz:
-                for i in range(len(baza)):
-                    baz.write(baza[i][0])
-                    if i != len(baza) - 1:
-                        baz.write('\n')
-            print('Продукт успешно удален из базы!')
+            print('Ошибка. Такой записи нет.')
     else:
         print('Ошибка. Неправильный ввод.')
     print()
