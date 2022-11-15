4e4ae4db135334c623c61ef9bbb04ded64051005
Pelmeshka <112972833+VeraKasianenko@users.noreply.github.com>
2022-11-15 22:16:20 +0300

    name can be empty

--- Programming/prog_dz/dz.py
+++ Programming/prog_dz/dz.py
@@ -694,7 +694,7 @@ def ch_name():
     with open('name.txt', 'r', encoding='UTF-8') as name:
         name = name.read()
     new_name = input('Введите новое имя, которое будет появляться при входе: ')
-    if new_name != '' and new_name != ' ' and new_name != '.' and new_name != ',':
+    if new_name != '.' and new_name != ',':
         if new_name != name:
             with open('name.txt', 'w', encoding='UTF-8') as name:
                 name.write(new_name)
