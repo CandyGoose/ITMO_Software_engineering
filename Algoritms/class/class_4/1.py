f8f32019f2f8f8ceaf523c61fe1c658b2823437b
Pelmeshka <112972833+VeraKasianenko@users.noreply.github.com>
2022-12-02 08:50:37 +0300

    alg class 4

---
+++ Algoritms/class/class_4/1.py
@@ -0,0 +1,19 @@
+def pr(str):
+    count = 0
+    cnt = 0
+    for i in str:
+        cnt += 1
+        if i == "(":
+            count += 1
+        elif i == ")":
+            count -= 1
+        if count < 0:
+            text = f'Ошибка в символе под номером: {cnt}'
+            return text
+    if count == 0:
+        text = 'Нет ошибки в вводе.'
+    else:
+        text = f'Ошибка в символе под номером: {cnt}'
+    return text
+str = input('Введите скобки: ')
+print(pr(str))
