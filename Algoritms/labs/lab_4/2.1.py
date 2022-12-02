d70a3d90326cc69da771470215e2a5b8f3b47559
Pelmeshka <112972833+VeraKasianenko@users.noreply.github.com>
2022-12-02 09:13:35 +0300

    Update 2.1.py

--- Algoritms/labs/lab_4/2.1.py
+++ Algoritms/labs/lab_4/2.1.py
@@ -3,28 +3,23 @@ def bucketSort(array):
     length = len(array)
     size = largest / length
 
-    # Create Buckets
     buckets = [[] for i in range(length)]
 
-    # Bucket Sorting
     for i in range(length):
         index = int(array[i] / size)
         if index != length:
             buckets[index].append(array[i])
         else:
             buckets[length - 1].append(array[i])
-
-    # Sorting Individual Buckets
+            
     for i in range(len(array)):
         buckets[i] = sorted(buckets[i])
 
-    # Flattening the Array
     result = []
     for i in range(length):
         result = result + buckets[i]
 
     return result
-
 
 arr = [5, 4, 2, 7, 8, 55, 2, 1, 4, 5, 1, 2]
 output = bucketSort(arr)
